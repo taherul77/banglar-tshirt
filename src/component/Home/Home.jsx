373b74bc71ded2a459b070dc235366b7cfa5407d
taherul77 <taherul2317@gmail.com>
2023-04-09 14:40:00 +0600

    update

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Home = () => {
+    const tshirt = useLoaderData();
+    return (
+        <div>
+            <div className="max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                <div className="px-4 py-2">
+                    <h1 className="">NIKE AI:{tshirt.length}</h1>
+                    <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quos quidem sequi illum facere recusandae voluptatibus</p>
+                </div>
+
+                <img className="object-cover w-full h-48 mt-2" src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=320&q=80" alt="NIKE AIR"></img>
+
+                <div className="flex items-center justify-between px-4 py-2 bg-gray-900">
+                    <h1 className="text-lg font-bold text-white">$129</h1>
+                    <button className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">Add to cart</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
